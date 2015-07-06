415b4725b48aee6ddf2435b5dba2145cbf3aaefd
LeeHyoun <idestroyi@naver.com>
2015-07-06 09:21:44 +0900

    update v.1

---
+++ board-yw/src/main/java/com/yw/bean/XmlDTO.java
@@ -0,0 +1,45 @@
+package com.yw.bean;
+
+public class XmlDTO {
+	String name;
+	String tel;
+	String address;
+	
+	public XmlDTO() {}
+	
+	// Data ---> XML 파일로 저장하기 위한 생성자.
+	public XmlDTO(String name, String tel, String address) {
+		
+		this.name = name;
+		this.tel = tel;
+		this.address = address;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "xmlDTO [name=" + name + ", tel=" + tel + ", address=" + address + "]";
+	}
+	
+	
+}
